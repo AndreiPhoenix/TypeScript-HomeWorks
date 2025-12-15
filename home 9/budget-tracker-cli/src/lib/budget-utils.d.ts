7a8e14a592dc82c31a6ffa39063e41b729b3a922
AndreiPhoenix <145689886+AndreiPhoenix@users.noreply.github.com>
2025-12-15 21:11:38 +0500

    budget-utils.d.ts

--- home 9/budget-tracker-cli/src/lib/budget-utils.d.ts
+++ home 9/budget-tracker-cli/src/lib/budget-utils.d.ts
@@ -1,1 +1,20 @@
+import { CategoryLimits } from '../interfaces/utility-types';
+import { ITransaction } from '../interfaces/ITransaction';
 
+export declare function checkCategoryLimits(
+  transactions: ITransaction[],
+  limits: CategoryLimits
+): boolean;
+
+export declare function filterTransactionsWithDescription(
+  transactions: ITransaction[]
+): ITransaction[];
+
+export declare function createTransactionsPreview(
+  transactions: ITransaction[]
+): Array<{
+  id: string;
+  amount: number;
+  type: import('../interfaces/TransactionType').TransactionType;
+  date: Date;
+}>;
